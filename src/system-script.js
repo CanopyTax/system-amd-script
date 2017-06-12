1e0480a8454ed9a387fbd05032376d07be734acc
Bret Little <bret.little@gmail.com>
2017-06-12 13:12:15 -0600

    Make it work with preloaded scripts

--- src/system-script.js
+++ src/system-script.js
@@ -21,6 +21,24 @@ function normalizeName(name) {
 	return name.indexOf("!") > -1 ? name.substring(0, name.indexOf("!")) : name;
 }
 
+function getScript(address) {
+	const existingScripts = Array.prototype.filter.call(
+		document.querySelectorAll("script"),
+		script => script.src === address
+	);
+
+	if (existingScripts.length) {
+		return existingScripts[0];
+	}
+
+	const head = document.getElementsByTagName("head")[0];
+	const script = document.createElement("script");
+	script.async = true;
+	script.src = address;
+	head.appendChild(script);
+	return script;
+}
+
 export function fetch(load) {
 	outerSystem = this;
 	// Prevent the default XHR request for the resource
@@ -32,9 +50,9 @@ export function fetch(load) {
 
 		if (address) resolve("");
 
-		const head = document.getElementsByTagName("head")[0];
-		const script = document.createElement("script");
-		script.async = true;
+		const script = getScript(load.address);
+		script.addEventListener("load", complete, false);
+		script.addEventListener("error", error, false);
 
 		function complete() {
 			if (
@@ -51,12 +69,6 @@ export function fetch(load) {
 		function error(evt) {
 			reject(new Error(`Error loading module from the address: "${load.address}"`));
 		}
-
-		script.addEventListener("load", complete, false);
-		script.addEventListener("error", error, false);
-
-		script.src = load.address;
-		head.appendChild(script);
 	});
 }
 
