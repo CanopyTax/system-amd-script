6f2c20e21cfabc10710e719c0b6e73a830b81035
Joel Denning <joel.denning@canopytax.com>
2018-01-03 15:45:57 -0700

    More tabs -> spaces

--- src/system-script-setup.js
+++ src/system-script-setup.js
@@ -1,23 +1,23 @@
 !function() {
-	window.__systemAmdScript = {};
+  window.__systemAmdScript = {};
 
-	let scriptNameMap = window.__systemAmdScript.scriptNameMap = {};
-	let nameCounter = 0;
+  let scriptNameMap = window.__systemAmdScript.scriptNameMap = {};
+  let nameCounter = 0;
 
-	window.define = window.canopyDefine = function(name, deps, m) {
-		const outerSystem = __systemAmdScript.SystemJS || SystemJS;
+  window.define = window.canopyDefine = function(name, deps, m) {
+    const outerSystem = __systemAmdScript.SystemJS || SystemJS;
 
-		if (typeof name === "string") {
-			nameCounter++;
-			const newName = `__samds__${name}__${nameCounter}`;
-			scriptNameMap[
-				name.indexOf("!") > -1 ? name.substring(0, name.indexOf("!")) : name
-			] = newName;
-			name = newName;
-		}
+    if (typeof name === "string") {
+      nameCounter++;
+      const newName = `__samds__${name}__${nameCounter}`;
+      scriptNameMap[
+        name.indexOf("!") > -1 ? name.substring(0, name.indexOf("!")) : name
+      ] = newName;
+      name = newName;
+    }
 
-		outerSystem.amdDefine(name, deps, m);
-	};
+    outerSystem.amdDefine(name, deps, m);
+  };
 
-	window.define.amd = true;
+  window.define.amd = true;
 }();
